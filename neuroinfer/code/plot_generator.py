d93c160d39e7c871bad13cf4bedd0766d8883347
mlionello <lionello.matteo@gmail.com>
2023-12-11 14:35:47 +0100

    added args check

--- neuroinfer/code/plot_generator.py
+++ neuroinfer/code/plot_generator.py
@@ -1,34 +1,28 @@
-import matplotlib.pyplot as plt
 from io import BytesIO
 import base64
 from neuroinfer.code.BayesianAnalysis import run_bayesian_analysis
 import numpy as np
 import datetime
 from pathlib import Path
-
-plt.switch_backend('Agg')
 import nibabel as nib
 from nilearn import image
 import os
+import matplotlib.pyplot as plt
+plt.switch_backend('Agg')
+
 
 def generate_plot(data):
-    # Extracting data from the form
-    brainRegion = data.get('brainRegion')
-    radius = data.get('radius')
-    x = data.get('x')
-    y = data.get('y')
-    z = data.get('z')
-    words = data.get('words')
-    priors = data.get('probabilities')
+    # Extracting data from the form:
+    cog_list, prior_list, x_target, y_target, z_target, radius, brainRegion = parse_input_ars(data)
 
-    # generating a nifti mask fro the selected region
+    # generating a nifti mask for the selected region
     generate_nifit_mask(brainRegion, './templates/atlases/HarvardOxford/HarvardOxford-cort-maxprob-thr25-2mm.nii.gz')
 
     #[coords, bf] = run_bayesian_analysis(brainRegion, words, radius, priors)
     #results = create_hist(coords, bf)
     #generate_nifti_bf(coords, bf)
 
-    plt.bar(words.split(','), [float(p) for p in priors.split(',')])
+    plt.bar(cog_list, [float(p) for p in prior_list])
 
     # Save the plot to a BytesIO object
     img_buffer = BytesIO()
@@ -74,3 +68,51 @@ def generate_nifti_bf(coords, bf, atlas_target_path):
     filename = f"{datetime.datetime.now():%Y%m%d_%H%M%S}"
     nib.save(bf_map_img, Path('.tmp') / filename)
     return
+
+
+def parse_input_ars(data):
+    brainRegion = data.get('brainRegion')
+    radius = data.get('radius')
+    x = data.get('x')
+    y = data.get('y')
+    z = data.get('z')
+    words = data.get('words')
+    words = words.split(',')
+    priors = data.get('probabilities')
+    priors = priors.split(',')
+    if (len(brainRegion)>0) & (str(type(brainRegion)) != "int"):
+        try:
+            brainRegion = np.int32(brainRegion)
+        except ValueError:
+            raise Exception(f"Cannot convert {brainRegion} to a int")
+    if (len(x)>0) & (str(type(x)) != "float"):
+        try:
+            x = float(x)
+        except ValueError:
+            raise Exception(f"Cannot convert {x} to a float")
+    if (len(y)>0) & (str(type(y)) != "float"):
+        try:
+            y = float(y)
+        except ValueError:
+            raise Exception(f"Cannot convert {y} to a float")
+    if (len(z)>0) & (str(type(z)) != "float"):
+        try:
+            z = float(z)
+        except ValueError:
+            raise Exception(f"Cannot convert {z} to a float")
+    if (len(radius)>0) & (str(type(radius)) != "float"):
+        try:
+            radius = float(radius)
+        except ValueError:
+            raise Exception(f"Cannot convert {radius} to a float")
+    if type(words) != list:
+        raise Exception(f"{words} of type {type(words)} is not a list")
+    if type(priors) != list:
+        raise Exception(f"{priors} of type {type(priors)} is not a list")
+    try:
+        priors = [np.int32(p) for p in priors]
+    except ValueError:
+        raise Exception(f"Cannot convert {priors} to a list of int")
+    if (len(priors) > 1) & (len(priors) != len(words)):
+        raise Exception(f"Illegal prior length {len(priors)} with words length {len(priors)}")
+    return words, priors, x, y, z, radius, brainRegion
