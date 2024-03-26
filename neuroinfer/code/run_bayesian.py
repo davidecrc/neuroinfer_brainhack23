974de7bf92699e5ad34b351642be4f2b32da2d83
mlionello <lionello.matteo@gmail.com>
2024-03-26 15:58:31 +0100

    fixed /0 error

--- neuroinfer/code/run_bayesian.py
+++ neuroinfer/code/run_bayesian.py
@@ -11,7 +11,7 @@ from PIL import Image
 from nilearn import image
 from scipy import sparse
 import json
-
+from neuroinfer import PKG_FOLDER
 import pickle
 
 '''
@@ -25,7 +25,9 @@ It offers two analysis pathways:
 The script calculates various statistics including likelihood, prior, posterior, and Bayesian Factor (BF).
 It uses the functions get_atlas_coordinates_json e get_distance to obtain the coordinates in an atlas and the distance between pair of coordinates.
 '''
-def run_bayesian_analysis_coordinates(cog_list, prior_list, x_target, y_target, z_target, radius, feature_df, cm): 
+
+
+def run_bayesian_analysis_coordinates(cog_list, prior_list, x_target, y_target, z_target, radius, feature_df, cm):
     frequency_threshold = 0.05
     t = time.time()
     cog_all, prior_all, ids_cog_nq_all, intersection_cog_nq_all, intersection_not_cog_nq_all = [], [], [], [], []
@@ -118,26 +120,25 @@ def run_bayesian_analysis_coordinates(cog_list, prior_list, x_target, y_target, 
         "radius": radius,
     }
 
-    #return df_data_all, cm, cog_all, prior_all, x_target, y_target, z_target, radius
-    return results        
-
+    # return df_data_all, cm, cog_all, prior_all, x_target, y_target, z_target, radius
+    return results
 
 
 def get_distance(coord1, coord2):
-    #print(np.linalg.norm(np.array(coord1) - np.array(coord2)))
+    # print(np.linalg.norm(np.array(coord1) - np.array(coord2)))
     return np.linalg.norm(np.array(coord1) - np.array(coord2))
-
 
 
 def get_atlas_coordinates_json(json_path):
     with open(json_path, 'r') as infile:
         coordinates_atlas = json.load(infile)
-    #for key, coordinates in coordinates_atlas.items():
-        #print(f"Key: {key}, Number of Coordinates: {len(coordinates)}")
+    # for key, coordinates in coordinates_atlas.items():
+    # print(f"Key: {key}, Number of Coordinates: {len(coordinates)}")
 
     return coordinates_atlas
 
-def run_bayesian_analysis_area(cog_list, prior_list, area, radius, feature_df,cm):
+
+def run_bayesian_analysis_area(cog_list, prior_list, area, radius, feature_df, cm):
     """
     Perform Bayesian analysis in a specified area using coordinates from the atlas.
 
@@ -152,62 +153,66 @@ def run_bayesian_analysis_area(cog_list, prior_list, area, radius, feature_df,cm
     - results: list of BF and coordinates.
     """
     # Extract coordinates of the specified area from the atlas
-     # Get the script's directory using Path
+    # Get the script's directory using Path
 
     # Set the global path
     # Get the directory of the current Python script
     script_directory = os.path.dirname(os.path.abspath(__file__))
     global_path = os.path.dirname(script_directory)
-    print('Global path: ',global_path)
+    print('Global path: ', global_path)
     data_path = os.path.join(global_path, "data")  # Path to the saved_result folder
-    print('Data path: ',data_path)
+    print('Data path: ', data_path)
 
     t_area = time.time()
 
-
     # Load coordinates from the JSON file
-    json_path = data_path+ "/coord_label_all_harv_ox.json"
+    json_path = data_path + "/coord_label_all_harv_ox.json"
     print('json_path:', json_path)
     coordinates_atlas = get_atlas_coordinates_json(json_path)
 
     # Extract coordinates specific to the provided area
     coordinates_area = coordinates_atlas.get(str(area), [])
-    print('len(coordinates_area) for this area: ' , len(coordinates_area))
-
+    print('len(coordinates_area) for this area: ', len(coordinates_area))
 
     # Rescale the radius to be between 2 and 4
     rescaled_radius = max(2, min(radius, 4))
-    print(' rescaled_radius:' ,  rescaled_radius)
+    print(' rescaled_radius:', rescaled_radius)
 
-    
     # Initialize an empty list to store results and coordinates
-    #results_dict = {}
+    # results_dict = {}
     result_all = []
-    coord=[]
-    
+    coord = []
+
     # Iterate through the coordinates_area
-    for i in range(len(coordinates_area)-1): #-1 in order to have the last coordinates_area[i+1]) #TODO improve
-        
+    for i in range(len(coordinates_area) - 1):  # -1 in order to have the last coordinates_area[i+1]) #TODO improve
+
         x_target, y_target, z_target = coordinates_area[i]
         coord.append([x_target, y_target, z_target])
-        #print(coord)
-        #print (i)
-        if i==0:
-            #result = run_bayesian_analysis_coordinates(cog_list, prior_list, x_target, y_target, z_target, rescaled_radius, feature_df,cm)
-            results = run_bayesian_analysis_coordinates(cog_list, prior_list, x_target, y_target, z_target, rescaled_radius, feature_df,cm)
-            #print(results_dict[i])           
+        # print(coord)
+        # print (i)
+        if i == 0:
+            # result = run_bayesian_analysis_coordinates(cog_list, prior_list, x_target, y_target, z_target, rescaled_radius, feature_df,cm)
+            results = run_bayesian_analysis_coordinates(cog_list, prior_list, x_target, y_target, z_target,
+                                                        rescaled_radius, feature_df, cm)
+            # print(results_dict[i])
             # Append a tuple containing the BF value and the coordinates to result_with_coordinates
             result_all.append(results)
-        #df_data_all, cm, cog_all, prior_all, x_target, y_target, z_target, radius
+        # df_data_all, cm, cog_all, prior_all, x_target, y_target, z_target, radius
 
         # Check if next_coord is distant > rescaled_radius * 0.98 from all previous coordinates
-        
-        #if all(get_distance((x_target, y_target, z_target), coordinate) > rescaled_radius * 0.98 for coordinate in coord):
-        if get_distance((x_target, y_target, z_target), coordinates_area[i+1]) > rescaled_radius: #TODO upload checking all the distance from the ROIS
-            print(f'Calculating cm for the {i+1} ROI out of {len(coordinates_area)}, {((i+1)/len(coordinates_area))*100}% ')
-            print(get_distance((x_target, y_target, z_target), coordinates_area[i+1]))    
+
+        # if all(get_distance((x_target, y_target, z_target), coordinate) > rescaled_radius * 0.98 for coordinate in coord):
+        if get_distance((x_target, y_target, z_target), coordinates_area[
+            i + 1]) > rescaled_radius:  # TODO upload checking all the distance from the ROIS
+            print(
+                f'Calculating cm for the {i + 1} ROI out of {len(coordinates_area)}, {((i + 1) / len(coordinates_area)) * 100}% ')
+            if i % 10 == 0:
+                with open(PKG_FOLDER / 'neuroinfer' / '.tmp' / 'processing_progress', 'w') as f_tmp:
+                    f_tmp.write(str((i + 1) / len(coordinates_area)))
+            print(get_distance((x_target, y_target, z_target), coordinates_area[i + 1]))
             # Call run_bayesian_analysis_coordinates with the current coordinates
-            results = run_bayesian_analysis_coordinates(cog_list, prior_list, x_target, y_target, z_target, rescaled_radius, feature_df,cm)
+            results = run_bayesian_analysis_coordinates(cog_list, prior_list, x_target, y_target, z_target,
+                                                        rescaled_radius, feature_df, cm)
             # Append a tuple containing the BF value and the coordinates to result_with_coordinates
             result_all.append(results)
 
@@ -215,13 +220,12 @@ def run_bayesian_analysis_area(cog_list, prior_list, area, radius, feature_df,cm
     print(f'Time in min: {round(elapsed_area / 60, 2)}')
 
     # Plot distribution and statistics of the Bayesian Factor (BF) for the area
-    #plot_distribution_statistics(result_with_coordinates)
+    # plot_distribution_statistics(result_with_coordinates)
 
     print(result_all)
-    
 
     return result_all
 
-#crea un volume con stesso volume del template
-#man mano che si scorrono le coordinate si mette a 0 l'intorno di quella coordinat con in input il raggio (occhio a considerare padding). Quando si considera la coordinata successiva, ripete il calcolo del BF solo se e' diversa da 0 
-#cosi' da non ripetere il calcolo del BF per coordinate vicine
+# crea un volume con stesso volume del template
+# man mano che si scorrono le coordinate si mette a 0 l'intorno di quella coordinat con in input il raggio (occhio a considerare padding). Quando si considera la coordinata successiva, ripete il calcolo del BF solo se e' diversa da 0
+# cosi' da non ripetere il calcolo del BF per coordinate vicine
