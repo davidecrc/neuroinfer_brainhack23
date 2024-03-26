b3e1968712ca06debf1ea6b7415401ce79f40bdf
mlionello <lionello.matteo@gmail.com>
2024-03-26 12:00:08 +0100

    introfucing padding template

--- neuroinfer/code/run_bayesian.py
+++ neuroinfer/code/run_bayesian.py
@@ -1,18 +1,10 @@
 # Standard Library Imports
 import os
 import time
-from argparse import ArgumentParser
-
 # Third-party Library Imports
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from PIL import Image
-from nilearn import image
-from scipy import sparse
 import json
-
-import pickle
 
 '''
 Script to run_bayesian_analysis_coordinates
@@ -117,16 +109,13 @@ def run_bayesian_analysis_coordinates(cog_list, prior_list, x_target, y_target, 
         "z_target": z_target,
         "radius": radius,
     }
-
-    #return df_data_all, cm, cog_all, prior_all, x_target, y_target, z_target, radius
-    return results        
+    return results
 
 
 
 def get_distance(coord1, coord2):
     #print(np.linalg.norm(np.array(coord1) - np.array(coord2)))
     return np.linalg.norm(np.array(coord1) - np.array(coord2))
-
 
 
 def get_atlas_coordinates_json(json_path):
@@ -152,7 +141,7 @@ def run_bayesian_analysis_area(cog_list, prior_list, area, radius, feature_df,cm
     - results: list of BF and coordinates.
     """
     # Extract coordinates of the specified area from the atlas
-     # Get the script's directory using Path
+    # Get the script's directory using Path
 
     # Set the global path
     # Get the directory of the current Python script
@@ -164,7 +153,6 @@ def run_bayesian_analysis_area(cog_list, prior_list, area, radius, feature_df,cm
 
     t_area = time.time()
 
-
     # Load coordinates from the JSON file
     json_path = data_path+ "/coord_label_all_harv_ox.json"
     print('json_path:', json_path)
@@ -174,54 +162,69 @@ def run_bayesian_analysis_area(cog_list, prior_list, area, radius, feature_df,cm
     coordinates_area = coordinates_atlas.get(str(area), [])
     print('len(coordinates_area) for this area: ' , len(coordinates_area))
 
-
     # Rescale the radius to be between 2 and 4
     rescaled_radius = max(2, min(radius, 4))
     print(' rescaled_radius:' ,  rescaled_radius)
 
-    
-    # Initialize an empty list to store results and coordinates
-    #results_dict = {}
     result_all = []
     coord=[]
-    
+    visited_coord = np.zeros(np.max(coordinates_area[0])-np.min(coordinates_area[0]),
+                             np.max(coordinates_area[1])-np.min(coordinates_area[1]),
+                             np.max(coordinates_area[2])-np.min(coordinates_area[2]))
+    coord_ranges={'xmax':np.max(coordinates_area[0]),'xmin' np.min(coordinates_area[0]),
+                             'ymax':np.max(coordinates_area[1]), 'ymin': np.min(coordinates_area[1]),
+                             'zmax':np.max(coordinates_area[2]),'zmin':np.min(coordinates_area[2])}
+
+    padding = int(radius/3)
     # Iterate through the coordinates_area
     for i in range(len(coordinates_area)-1): #-1 in order to have the last coordinates_area[i+1]) #TODO improve
-        
         x_target, y_target, z_target = coordinates_area[i]
         coord.append([x_target, y_target, z_target])
-        #print(coord)
-        #print (i)
         if i==0:
-            #result = run_bayesian_analysis_coordinates(cog_list, prior_list, x_target, y_target, z_target, rescaled_radius, feature_df,cm)
             results = run_bayesian_analysis_coordinates(cog_list, prior_list, x_target, y_target, z_target, rescaled_radius, feature_df,cm)
-            #print(results_dict[i])           
             # Append a tuple containing the BF value and the coordinates to result_with_coordinates
             result_all.append(results)
-        #df_data_all, cm, cog_all, prior_all, x_target, y_target, z_target, radius
 
-        # Check if next_coord is distant > rescaled_radius * 0.98 from all previous coordinates
-        
-        #if all(get_distance((x_target, y_target, z_target), coordinate) > rescaled_radius * 0.98 for coordinate in coord):
-        if get_distance((x_target, y_target, z_target), coordinates_area[i+1]) > rescaled_radius: #TODO upload checking all the distance from the ROIS
-            print(f'Calculating cm for the {i+1} ROI out of {len(coordinates_area)}, {((i+1)/len(coordinates_area))*100}% ')
-            print(get_distance((x_target, y_target, z_target), coordinates_area[i+1]))    
+        if ~is_visited((x_target, y_target, z_target), visited_coord):
+            print(
+                f'Calculating cm for the {i + 1} ROI out of {len(coordinates_area)}, {((i + 1) / len(coordinates_area)) * 100}% ')
+            print(get_distance((x_target, y_target, z_target), coordinates_area[i + 1]))
             # Call run_bayesian_analysis_coordinates with the current coordinates
-            results = run_bayesian_analysis_coordinates(cog_list, prior_list, x_target, y_target, z_target, rescaled_radius, feature_df,cm)
+            results = run_bayesian_analysis_coordinates(cog_list, prior_list, x_target, y_target, z_target,
+                                                        rescaled_radius, feature_df, cm)
             # Append a tuple containing the BF value and the coordinates to result_with_coordinates
             result_all.append(results)
+            visited_coord = update_visited_coord(visited_coord, normalize_coord(x_target, y_target, z_target, coord_ranges), padding)
+
 
     elapsed_area = time.time() - t_area
     print(f'Time in min: {round(elapsed_area / 60, 2)}')
 
     # Plot distribution and statistics of the Bayesian Factor (BF) for the area
     #plot_distribution_statistics(result_with_coordinates)
-
     print(result_all)
-    
 
     return result_all
 
 #crea un volume con stesso volume del template
 #man mano che si scorrono le coordinate si mette a 0 l'intorno di quella coordinat con in input il raggio (occhio a considerare padding). Quando si considera la coordinata successiva, ripete il calcolo del BF solo se e' diversa da 0 
 #cosi' da non ripetere il calcolo del BF per coordinate vicine
+
+def normalize_coord(x_target, y_target, z_target, coord_ranges):
+    xnorm = x_target - coord_ranges['xmin']
+    ynorm = y_target - coord_ranges['xmin']
+    znorm = z_target - coord_ranges['xmin']
+    return xnorm, ynorm, znorm
+
+def visited_coord(coord, visited_coords):
+    if any(visited_coords[coord]) == 1:
+        return True
+    return False
+
+def update_visited_coord(visited_coords, x_norm, y_norm, z_norm, padding):
+    coords_visited = x_norm, y_norm, z_norm + padding
+    visited_coords[coords_visited] = 1
+    return coords_visited
+
+def is_visited(current_coord, visited_coords):
+    return visited_coords[current_coord[0], current_coord[1], current_coord[2]]
