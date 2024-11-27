74ed23420ab3c218fa3d74999b49342888863e36
asddat <lionello.matteo@gmail.com>
2024-11-27 18:56:39 +0100

    blacked

--- neuroinfer/code/python/utils.py
+++ neuroinfer/code/python/utils.py
@@ -70,9 +70,15 @@ def get_sphere_coords(coords, vx_radius, overlay_results):
 
     vx_radius = int(vx_radius)
     # Define bounds within vx_radius to avoid generating full meshgrid
-    x_min, x_max = max(0, coords[0] - vx_radius), min(volume_shape[0], coords[0] + vx_radius + 1)
-    y_min, y_max = max(0, coords[1] - vx_radius), min(volume_shape[1], coords[1] + vx_radius + 1)
-    z_min, z_max = max(0, coords[2] - vx_radius), min(volume_shape[2], coords[2] + vx_radius + 1)
+    x_min, x_max = max(0, coords[0] - vx_radius), min(
+        volume_shape[0], coords[0] + vx_radius + 1
+    )
+    y_min, y_max = max(0, coords[1] - vx_radius), min(
+        volume_shape[1], coords[1] + vx_radius + 1
+    )
+    z_min, z_max = max(0, coords[2] - vx_radius), min(
+        volume_shape[2], coords[2] + vx_radius + 1
+    )
 
     # Create grids within this smaller bounding box
     # X, Y, Z = np.ogrid[x_min:x_max, y_min:y_max, z_min:z_max]
@@ -93,13 +99,15 @@ def get_sphere_coords(coords, vx_radius, overlay_results):
     # squared_distances2 = (X - coords[0]) ** 2 + (Y - coords[1]) ** 2 + (Z - coords[2]) ** 2
 
     # Mask where the squared distance is within the square of the radius
-    template_3D = squared_distances2 <= (vx_radius ** 2)
+    template_3D = squared_distances2 <= (vx_radius**2)
 
     # Get the indices within the sphere and adjust for bounding box offset
     template_indices = np.nonzero(template_3D)
-    sphere_coords = (template_indices[0] + x_min,
-                     template_indices[1] + y_min,
-                     template_indices[2] + z_min)
+    sphere_coords = (
+        template_indices[0] + x_min,
+        template_indices[1] + y_min,
+        template_indices[2] + z_min,
+    )
 
     return sphere_coords
 
